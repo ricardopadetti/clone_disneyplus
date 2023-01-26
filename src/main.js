d44640c7a77f497390405db126b792315a8822d4
Ricardo Padetti <ricardopadetti@gmail.com>
2023-01-26 20:21:36 -0300

    ajuste mobile

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader()
+        }
+    })
+
+
+// seção de atrações, programação abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -15,11 +29,23 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-
+// seção faq, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
